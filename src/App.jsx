ba334573a5926df57628518d8aaea077baafb5e7
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-02-18 11:46:42 +0100

    Add Popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
+import {
+  Container, Header, Input, Button,
+} from 'semantic-ui-react';
 
- const App = () => {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
-}
+const App = () => (
+  <Container>
+    <Header as="h4">GeoBattle</Header>
+    <Button primary>Create Challenge</Button>
+    <Input placeholder="Enter Challenge Link..." />
+    <Button attached="right">Send</Button>
+  </Container>
+);
 
 export default App;
